281bd5f50d8a9c5cd11de737ebc649c5fc21d7c1
System Administrator <devsstudent777@gmail.com>
2022-05-19 11:36:13 +0200

    resume

---
+++ roadmp.c
@@ -0,0 +1,27 @@
+//first agrs : string
+//Second args : all value to replace into the string 
+
+//ft_check -> '%' count	(not count double one) / compare it to args count
+
+//ft_movesin_and_create: parcourir la chaine et faire appel a grab it a chaque %
+
+//ft_check_grab and ft_grabit : conditionner la recuperation des flags (ni trop ni pas assez, surement des ifs ...)
+
+//ft_check_args and type (
+
+//faires des checker pour tous les flags / en fonction de la valeur de retour comportement different
+
+Need to calcul size of value in args (for int for examle)
+Convert to hexa function to
+
+strange conversion to hexa when its negative number (ffff)
+
+dontprint unprintable char
+
+space one time (like "% I" same space as "%      i")
+0sizetype -> si size > size number -> comble avec des 0
+-sizetype -> si size > size number -> comble a droite avec des espaces
+# -> 0x devant les hexas !
+.sizeype afiche seulement les sizepremiers charchet (comble pas)
++type afiche un character +devant le nombre
+
